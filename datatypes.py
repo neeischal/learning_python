1b389eb08e39e06ec92742ace5a624c3f77f0abe
NischalKharel3 <>
2021-10-11 03:13:43 +0000

    Initial commit

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+string = "Hello World " + str(123)
+integer = 123 + int(str1_number)
+floating = 1.334
+boolean = True
+character = '$' + "adsfsa"
+
+print(type(string))
+print(type(integer), "=", integer )
+print(type(floating))
+print(type(boolean))
+print(type(character))
---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("Hi,what is your name? ")
+print("hello " + name)
+age=input("what is your age? ")
+print("it means you are " + age)
