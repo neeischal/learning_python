eb26ac9ce18056ef053c13b6f1d45597a7333f70
Nischal Kharel <92285138+neeischal@users.noreply.github.com>
2021-10-22 13:27:12 +0000

    added new programs and modify some of the old programs

--- ageprogram.py
+++ ageprogram.py
@@ -4,7 +4,5 @@ age=int(age)
 
 if(age<18):
   print("you are a bachaa")
-elif(age>=18 and age<=21 ):
-  print("you are barely an adult")
-elif(age>21):
+elif(age>=18):
   print("you are an adult")
---
+++ positionfinder.py
@@ -0,0 +1,5 @@
+a=input('word for finding position')
+i=-1
+for x in a:
+  print(x,i)
+  i=i+1
